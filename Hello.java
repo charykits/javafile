a97fbd4415fce2aa00001d5fa0eefde5a7971f94
chary <veerabrahma1234.chary@gmail.com>
2025-09-10 15:40:57 +0530

    javafile

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello
+{
+public static void main(String[] args)
+{
+String name = System.getenv("var1");
+if(name!=null) {
+System.out.println("Hello, " + name);
+}
+else {
+System.out.println("Environment variable is not set in jenkins");
+}
+}
+}
