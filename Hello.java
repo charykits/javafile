f31a4bbea8462f4986a371eaf728180a2603df9b
charykits <veerabrahma1234.chary@gmail.com>
2025-09-10 15:51:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
 public static void main(String[] args)
 {
 String name = System.getenv("var1");
-if(name!=null) {
+if(name!=NULL) {
 System.out.println("Hello, " + name);
 }
 else {
