1add70cc005483ff17c6630c904a78ad41b22773
charykits <veerabrahma1234.chary@gmail.com>
2025-09-11 15:50:25 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,6 +5,6 @@ public static void main(String[] args)
   String num1 = System.getenv(num1);
   String num2 = System.getenv(num2);
   int result = Integer.parseInt(num1) + Integer.parseInt(num2);
-  System.out.println(sum);
+  System.out.println(result);
 }
 }
