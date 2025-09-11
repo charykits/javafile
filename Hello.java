cbadf791bd337cb11fdb74daa3038e81c58f7fc1
charykits <veerabrahma1234.chary@gmail.com>
2025-09-11 15:49:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,10 +2,9 @@ public class Hello
 {
 public static void main(String[] args)
 {
-  String num1 = System.getenv("num1");
-  String num2 = System.getenv("num2");
-  String result = Integer.parseInt(num1) + Integer.parseInt(num2);
-  int sum = Integer.parseInt(result);
-System.out.println(sum);
+  String num1 = System.getenv(num1);
+  String num2 = System.getenv(num2);
+  int result = Integer.parseInt(num1) + Integer.parseInt(num2);
+  System.out.println(sum);
 }
 }
