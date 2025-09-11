c4af8d6b55f270d2fc787521fd8ef21a795a8739
charykits <veerabrahma1234.chary@gmail.com>
2025-09-11 13:23:54 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ public class Hello
 {
 public static void main(String[] args)
 {
-  int num1 = System.getenv("num1");
-  int num2 = System.getenv("num2");
+  int num1 = System.getenv(num1);
+  int num2 = System.getenv(num2);
   int result = num1 + num2;
 System.out.println(result);
 }
