18de369adb9d57a5ae0cd49fa8c7f166fd3becf6
charykits <veerabrahma1234.chary@gmail.com>
2025-09-11 13:18:49 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,12 +2,9 @@ public class Hello
 {
 public static void main(String[] args)
 {
-String name = System.getenv("var1");
-if(name != null) {
-System.out.println("Hello, " + name);
-}
-else {
-System.out.println("Environment variable is not set in jenkins");
-}
+  int num1 = System.getenv("num1");
+  int num2 = System.getenv("num2");
+  int result = num1 + num2;
+System.out.println(result);
 }
 }
